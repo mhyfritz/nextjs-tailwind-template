f93c53942d6a7aceec4ad4cf950b74323490c14c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2020-07-11 06:51:15 +0200

    rename classes

--- components/button.js
+++ components/button.js
@@ -1,21 +1,21 @@
 export default function Button({ children, ...props }) {
   return (
     <>
-      <button {...props} className="button button--blue">
+      <button {...props} className="btn btn-blue">
         {children}
       </button>
 
       <style jsx>{`
-        .button {
+        .btn {
           @apply px-6 py-2 font-semibold border;
         }
 
-        .button--blue {
+        .btn-blue {
           @apply text-blue-500 border-blue-500;
         }
 
-        .button--blue:hover,
-        .button--blue:focus {
+        .btn-blue:hover,
+        .btn-blue:focus {
           @apply text-white bg-blue-700 border-blue-700;
         }
       `}</style>
