ba7d245368452bc28c5c82da8eb27ba2676fe22f
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2020-07-28 06:25:09 +0200

    extend purgecss filename patterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
